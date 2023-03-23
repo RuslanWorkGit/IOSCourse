798f3cb029582d860368ff8d07cc91a9e7ad7dfc
RuslanWorkGit <lulka758@gmail.com>
2023-03-23 21:19:11 +0200

    done task 5-6 in homework2

--- HomeWork/Second/HomeWork2.playground/Contents.swift
+++ HomeWork/Second/HomeWork2.playground/Contents.swift
@@ -55,7 +55,62 @@ titanic.introduceAll()
 
 //MARK: Task-5
 
-class Claculator {
+class Calculator {
     
+    func add(numOne: Double, numTwo: Double) -> Double {
+        return numOne + numTwo
+    }
     
+    func minus(numOne: Double, numTwo: Double) -> Double {
+        return numOne - numTwo
+    }
+    
+    func mult(numOne: Double, numTwo: Double) -> Double {
+        return numOne * numTwo
+    }
+    
+    func divide(numOne: Double, numTwo: Double) -> Double {
+        return numOne / numTwo
+    }
 }
+
+class CalculatorPro: Calculator {
+    
+    func calculatePercent(num: Double, percent: Double) -> Double {
+        let onePercent = num / 100
+        return percent * onePercent
+    }
+    
+    func myPow(num: Double, power: Double) -> Double {
+        return pow(num, power)
+    }
+}
+
+class SuperProCalc: CalculatorPro {
+    
+    func calculateDepositAmount(principal: Double, interestRate: Double, years: Double) -> Double {
+        let interest = interestRate / 100.0
+        let totalInterest = pow((1 + interest), years)
+        let amount = principal * totalInterest
+        return amount
+    }
+}
+
+let myCalc = SuperProCalc()
+
+myCalc.calculateDepositAmount(principal: 100000, interestRate: 12.5, years: 2)
+
+
+//MARK: Task-6
+
+func battaryLife(battPercent: Int) {
+    switch battPercent {
+    case 100: print("Battery is full")
+    case 70...80: print("You will need charger in 6 hour")
+    case 20...40: print("Put your device to charger")
+    case 0: print("Your device dead but if you use charger it can help")
+    default: print("unnown number")
+    }
+}
+
+battaryLife(battPercent: 34)
