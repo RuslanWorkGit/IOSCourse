cdc85a51ebaebc1344824f75dede4a7a4b0f2500
RuslanWorkGit <lulka758@gmail.com>
2024-10-09 08:40:01 +0300

    try without submodule

---
+++ Live/Live-7/Live-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live-7
+//
+//  Created by Ruslan Liulka on 09.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
