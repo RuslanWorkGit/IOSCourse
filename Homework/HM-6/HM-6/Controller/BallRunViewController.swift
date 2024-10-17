03e1fed5e75fc9f2c9f65cf6208982950858eaac
RuslanWorkGit <lulka758@gmail.com>
2024-10-17 19:21:11 +0300

    done 2 screen

--- Homework/HM-6/HM-6/Controller/BallRunViewController.swift
+++ Homework/HM-6/HM-6/Controller/BallRunViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class BallRunViewController: UIViewController {
 
+    let centralButton = UIButton()
+    let ball = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        centralButton.setTitle("Run!", for: .normal)
+        centralButton.frame = CGRect(x: (view.bounds.width - 200) / 2, y: view.bounds.height - 150, width: 200, height: 50)
+        centralButton.center = view.center
+        centralButton.setTitleColor(.black, for: .normal)
+        view.addSubview(centralButton)
+        
+        centralButton.addTarget(self, action: #selector(runBall), for: .touchUpInside)
+        
+        let startPosition = CGPoint(x: self.view.bounds.minX + 24, y: self.view.bounds.minY + 32)
+        ball.frame = CGRect(x: startPosition.x, y: startPosition.y,
+                            width: 50, height: 50)
+        ball.backgroundColor = .systemOrange
+        ball.layer.cornerRadius = 25
+        view.addSubview(ball)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @objc func runBall() {
+        UIView.animateKeyframes(withDuration: 5.0, delay: 0.0, options: [.calculationModeLinear]) {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2) {
+                self.ball.center.x = self.view.bounds.maxX - 24 - self.ball.bounds.width / 2
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2) {
+                self.ball.center.y = self.view.bounds.maxY - 32 - self.ball.bounds.height / 2
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.2) {
+                self.ball.center.x = self.view.bounds.minX + 24 + self.ball.bounds.width / 2
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.2) {
+                self.ball.center.y = self.view.bounds.minY + 32 + self.ball.bounds.width / 2
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.2) {
+                self.ball.alpha = 0
+            }
+        } completion: { finished in
+            print("Animated finished!")
+        }
     }
-    */
 
 }
