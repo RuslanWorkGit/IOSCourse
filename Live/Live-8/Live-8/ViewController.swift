8a904b01a88109029d3761a17c6ea45d29c445c0
RuslanWorkGit <lulka758@gmail.com>
2024-11-10 12:01:19 +0200

    Live-8 start

---
+++ Live/Live-8/Live-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live-8
+//
+//  Created by Ruslan Liulka on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
